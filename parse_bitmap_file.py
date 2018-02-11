5018aaa5f5ff8080f0fdad482076007cd9db70de
gnk263 <gnk263@users.noreply.github.com>
2018-02-12 00:35:13 +0900

    rgb文字列の表示形式を16進数にする

--- parse_bitmap_file.py
+++ parse_bitmap_file.py
@@ -12,7 +12,7 @@ def main():
     for y in range(height):
         for x in range(width):
             r, g, b = rgb.getpixel((x,y))
-            print("r:{0}, g:{1}, b:{2}".format(r, g, b))
+            print("r:{0}, g:{1}, b:{2}".format(hex(r), hex(g), hex(b)))
 
 if __name__ == "__main__":
     main()
