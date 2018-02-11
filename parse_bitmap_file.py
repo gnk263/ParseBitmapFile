bc99949182d98f30dd17cdcacb060e806984a104
gnk263 <gnk263@users.noreply.github.com>
2018-02-12 00:31:52 +0900

    bitmapファイルのr,g,bを出力する

--- parse_bitmap_file.py
+++ parse_bitmap_file.py
@@ -2,8 +2,17 @@
 from PIL import Image
 
 def main():
-    pass
+    bmp = Image.open("sample.bmp")
+    width, height = bmp.size
 
+    rgb = bmp.convert("RGB")
+
+    print("width:{0}, height:{1}".format(width, height))
+
+    for y in range(height):
+        for x in range(width):
+            r, g, b = rgb.getpixel((x,y))
+            print("r:{0}, g:{1}, b:{2}".format(r, g, b))
 
 if __name__ == "__main__":
     main()
